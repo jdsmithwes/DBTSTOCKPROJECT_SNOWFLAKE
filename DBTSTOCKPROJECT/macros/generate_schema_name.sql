a211b73e529c9c688f7a5f80a1b88c5cf706a6cd
Jamaal Smith <jdsmithwes@protonmail.com>
2026-04-05 17:22:15 +0000

    updates made to prefacing of layers for databases

--- DBTSTOCKPROJECT/macros/generate_schema_name.sql
+++ DBTSTOCKPROJECT/macros/generate_schema_name.sql
@@ -1,8 +1,17 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-  {# If +schema is set (staging/intermediate/marts), use it as-is #}
-  {%- if custom_schema_name is not none -%}
-    {{ custom_schema_name | upper }}
-  {%- else -%}
-    {{ target.schema | upper }}
-  {%- endif -%}
+{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
+
+    {%- if node.fqn[1] == 'staging' -%}
+        JDS_STG_{{ node.name }}
+
+    {%- elif node.fqn[1] == 'intermediate' -%}
+        JDS_INT_{{ node.name }}
+
+    {%- elif node.fqn[1] == 'marts' -%}
+        JDS_MART_{{ node.name }}
+
+    {%- else -%}
+        {{ node.name }}
+
+    {%- endif -%}
+
 {%- endmacro %}
